b6de3217329b0d3053e56317514ae940beb0702c
Alisa <alisakovalhome@mail.ru>
2022-12-19 20:21:32 +0300

    Add new solution of task4

--- task4/task4.js
+++ task4/task4.js
@@ -1,50 +1,3 @@
-
-function addNote() {
-    let noteCounter = localStorage.length
-    let newNote = document.querySelector('#newNote');
-
-    if (newNote.validity.valueMissing) {    
-        document.querySelector('#newNote').placeholder = 'вы ничего не ввели'        
-    }
-
-    if (!newNote.validity.valueMissing) {
-        let newNoteFormat = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
-        if (!newNote.value.match(newNoteFormat)) {
-            document.querySelector('#newNote').placeholder = 'неверный формат'           
-        } else {
-            localStorage.setItem(noteCounter, newNote.value)
-            document.querySelector('#newNote').placeholder = ''
-        }
-    }
-    
-    document.querySelector('#newNote').value = ''
-
-    generateNote()
-}
-
-function generateNote() {
-    let newString = '';
-    for (let itemKey = 0; localStorage.getItem(itemKey) != null; itemKey++) {
-        const note = localStorage.getItem(itemKey)
-        newString += `<div id="optional"><input type="checkbox"><span>${note}</span><button id="del" onclick="deleteNote(this)">Удалить</button></div>`;
-    }
-    document.querySelector('#container').innerHTML = newString;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    generateNote()
-})
-
-
-function deleteNote() {
-    let optional = document.querySelector('#optional')
-    optional.classList.toggle('hidden') 
-}
-
-
-
-// это работает, надо еще валидацию добавить
-
 let notes = []
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -56,8 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function addNote() {
-    let newNote = document.querySelector('#newNote').value;
-    notes.push(newNote)
+    let newNote = document.querySelector('#newNote');
+    if (newNote.validity.valueMissing) {    
+        document.querySelector('#newNote').placeholder = 'вы ничего не ввели'        
+    }
+
+    if (!newNote.validity.valueMissing) {
+        let newNoteFormat = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
+        if (!newNote.value.match(newNoteFormat)) {
+            document.querySelector('#newNote').placeholder = 'неверный формат'           
+        } else {
+            notes.push(newNote.value)
+            generateNote()
+            localStorage.setItem('MyNewNote', notes)
+            document.querySelector('#newNote').placeholder = ''
+        }
+    }
 
     document.querySelector('#newNote').value = ''
 
@@ -68,9 +35,6 @@ function addNote() {
     if (localStorage.getItem('MyNewNote') != null) {                
             localStorage.setItem('MyNewNote', notes)                 
     }
-
-    generateNote()
-
 }
 
 function generateNote() {
@@ -85,3 +49,53 @@ function deleteNote() {
     let optional = document.querySelector('#optional')
     optional.classList.toggle('hidden') 
 }
+
+
+
+// Тоже работает, но код без массивов
+// function addNote() {
+//     let noteCounter = localStorage.length
+//     let newNote = document.querySelector('#newNote');
+
+//     if (newNote.validity.valueMissing) {    
+//         document.querySelector('#newNote').placeholder = 'вы ничего не ввели'        
+//     }
+
+//     if (!newNote.validity.valueMissing) {
+//         let newNoteFormat = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
+//         if (!newNote.value.match(newNoteFormat)) {
+//             document.querySelector('#newNote').placeholder = 'неверный формат'           
+//         } else {
+//             localStorage.setItem(noteCounter, newNote.value)
+//             document.querySelector('#newNote').placeholder = ''
+//         }
+//     }
+    
+//     document.querySelector('#newNote').value = ''
+
+//     generateNote()
+// }
+
+// function generateNote() {
+//     let newString = '';
+//     for (let itemKey = 0; localStorage.getItem(itemKey) != null; itemKey++) {
+//         const note = localStorage.getItem(itemKey)
+//         newString += `<div id="optional"><input type="checkbox"><span>${note}</span><button id="del" onclick="deleteNote(this)">Удалить</button></div>`;
+//     }
+//     document.querySelector('#container').innerHTML = newString;
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     generateNote()
+// })
+
+
+// function deleteNote() {
+//     let optional = document.querySelector('#optional')
+//     optional.classList.toggle('hidden') 
+// }
+
+
+
+// это работает, надо еще валидацию добавить
+
